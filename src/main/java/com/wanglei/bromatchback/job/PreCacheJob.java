ea051341e7846e2aa8c126b762b1b0a195ee14c9
muqiu <811785134@qq.com>
2024-03-12 18:59:49 +0800

    Redisson实现分布式锁

--- src/main/java/com/wanglei/bromatchback/job/PreCacheJob.java
+++ src/main/java/com/wanglei/bromatchback/job/PreCacheJob.java
@@ -7,6 +7,8 @@ import com.wanglei.bromatchback.model.domain.User;
 import com.wanglei.bromatchback.service.UserService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -28,22 +30,37 @@ public class PreCacheJob {
     private RedisTemplate<String, Object> redisTemplate;
 
     @Resource
+    private RedissonClient redissonClient;
+
+    @Resource
     private UserService userService;
     private List<Long> mainUser = Arrays.asList(1L);
 
     @Scheduled(cron = "0 0 0 * * *")
     public void doCache() {
+        RLock lock = redissonClient.getLock("bromatch:PreCacheJob:doCache:lock");
+        try {
+            if (lock.tryLock(0, -1, TimeUnit.MILLISECONDS)) {
+                for (Long userId : mainUser) {
+                    String redisKey = String.format("bromatch:recommend:%s", userId);
+                    ValueOperations<String, Object> ValueOperations = redisTemplate.opsForValue();
+                    QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+                    Page<User> userPage = userService.page(new Page<>(1, 20), queryWrapper);
+                    //写缓存
+                    try {
+                        ValueOperations.set(redisKey, userPage, 5, TimeUnit.MINUTES);
+                    } catch (Exception e) {
+                        log.error("redis set key error", e);
 
-        for (Long userId : mainUser) {
-            String redisKey = String.format("bromatch:recommend:%s",userId);
-            ValueOperations<String, Object> ValueOperations = redisTemplate.opsForValue();
-            QueryWrapper<User> queryWrapper = new QueryWrapper<>();
-            Page<User> userPage = userService.page(new Page<>(1, 20), queryWrapper);
-            //写缓存
-            try {
-                ValueOperations.set(redisKey,userPage,5, TimeUnit.MINUTES);
-            } catch (Exception e) {
-                log.error("redis set key error",e);
+                    }
+                }
+
+            }
+        } catch (InterruptedException e) {
+            log.error("doCache error",e);
+        }finally {
+            if (lock.isHeldByCurrentThread()) {
+                lock.unlock();
             }
         }
 
